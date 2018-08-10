ef4b01ea5281d5c1f7dfad6358080c0fc935b983
mbarekh <hazem.mbarek@hotmail.com>
2018-08-10 11:17:35 +0200

    update .gitignore and build.nbat file

--- build.bat
+++ build.bat
@@ -1,2 +1,3 @@
 .\bin\grafana-cli --pluginsDir .\data\plugins plugins install grafana-simple-json-datasource
 .\bin\grafana-cli --pluginsDir .\data\plugins plugins install grafana-piechart-panel
+git clone https://github.com/mbarekh/grafana-diagram .\data\plugins\jdbranham-diagram-panel
